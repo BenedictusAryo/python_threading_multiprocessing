9e81e568ad1fc1ad544a7292a67a046cae6ad453
unknown <benedictusaryo@outlook.com>
2019-09-25 17:42:32 +0700

    add syncronuspy

---
+++ 1_Syncronuspy.py
@@ -0,0 +1,21 @@
+# 1 Simple Syncronus Python
+
+import time
+
+start = time.perf_counter()
+
+
+# Create simple function that sleep in 1 second
+def do_something():
+    print('Sleeping in 1 second ..')
+    time.sleep(1)
+    print('Done Sleeping')
+
+
+# Run the function twice, so expected run in 2s (@1 sec)
+do_something()
+do_something()
+
+finish = time.perf_counter()
+
+print(f"Finished in {round(finish-start,2)} second(s)")

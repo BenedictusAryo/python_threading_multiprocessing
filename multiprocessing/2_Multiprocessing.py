f0e709d189d781a57fc1ddb9d8484006293eee2c
unknown <benedictusaryo@outlook.com>
2019-10-04 12:23:25 +0700

    start multiprocessing

---
+++ multiprocessing/2_Multiprocessing.py
@@ -0,0 +1,29 @@
+# 2 Using multi processing Python
+
+import multiprocessing
+# Import Time and start counter
+import time
+start = time.perf_counter()
+
+
+# Create simple function that sleep in 1 second
+def do_something():
+    print('Sleeping 1 second ..')
+    time.sleep(1)
+    print('Done Sleeping')
+
+
+# Create 2 multiprocessing and select function as target
+p1 = multiprocessing.Process(target=do_something)
+p2 = multiprocessing.Process(target=do_something)
+
+p1.start()
+p2.start()
+
+p1.join()
+p2.join()
+p1.close()
+p2.close()
+# Finish counting and show script runtime
+finish = time.perf_counter()
+print(f"Finished in {round(finish-start,2)} second(s)")

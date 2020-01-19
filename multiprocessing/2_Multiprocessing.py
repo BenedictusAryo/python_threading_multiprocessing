2c148b135d9ebb7ff308bd6b0c89ff3fd129bdfa
BenedictusAryo <benedictusaryo@outlook.com>
2020-01-19 18:12:08 +0700

    multiprocessing using concurrent futures

--- multiprocessing/2_Multiprocessing.py
+++ multiprocessing/2_Multiprocessing.py
@@ -1,27 +1,28 @@
 # 2 Using multi processing Python
-
 import time
 import multiprocessing
 
-start = time.perf_counter()
-
 # Create simple function that sleep in 1 second
 def do_something():
-    print('Sleeping 1 second ..')
-    time.sleep(1)
-    print('Done Sleeping')
+        print('Sleeping 1 second ..')
+        time.sleep(1)
+        print('Done Sleeping')
+    
 
+if __name__ == '__main__':
 
-# Create 2 multiprocessing and select function as target
-p1 = multiprocessing.Process(target=do_something)
-p2 = multiprocessing.Process(target=do_something)
+    start = time.perf_counter()
 
-p1.start()
-p2.start()
+    # Create 2 multiprocessing and select function as target
+    p1 = multiprocessing.Process(target=do_something)
+    p2 = multiprocessing.Process(target=do_something)
 
-p1.join()
-p2.join()
+    p1.start()
+    p2.start()
 
-# Finish counting and show script runtime
-finish = time.perf_counter()
-print(f"Finished in {round(finish-start,2)} second(s)")
+    p1.join()
+    p2.join()
+
+    # Finish counting and show script runtime
+    finish = time.perf_counter()
+    print(f"Finished in {round(finish-start, 2)} second(s)")
---
+++ multiprocessing/3_Multiprocessing_loop.py
@@ -0,0 +1,29 @@
+# 3 Combine Multiprocessing with Loop
+import time
+import multiprocessing
+
+# Create simple function that sleep in 1 second
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s) ..')
+    time.sleep(seconds)
+    print('Done Sleeping')
+
+if __name__ == '__main__':
+    # Start counting
+    start = time.perf_counter()
+
+    # Create List for each processing
+    processes = []
+    # Loop n times, for creating multiproces then add to list
+    for _ in range(10):
+        p = multiprocessing.Process(target=do_something, args=[2])
+        p.start()
+        processes.append(p)
+
+    # Loop for processes then join each process
+    for process in processes:
+        process.join()
+
+    # Finish counting and show script runtime
+    finish = time.perf_counter()
+    print(f"Finished in {round(finish-start, 2)} second(s)")
